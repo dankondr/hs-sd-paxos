b12e0e12ac6394641309d09d2427c7b47bafda70
Daniil Kondratev <dankondr@yandex-team.ru>
2022-12-27 01:42:48 +0300

    More progress but unfinished ;(

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,16 @@
 
 module Main where
 
-import Control.Concurrent (forkIO)
+import Control.Concurrent (MVar, forkIO, newMVar, putMVar, takeMVar)
+import Control.Lens ((&), (.~), (^.))
 import Control.Monad (forever)
-import Data.Aeson (FromJSON, ToJSON)
+import Control.Monad.IO.Class ( MonadIO(liftIO) )
+import Data.Aeson (FromJSON, ToJSON, Value)
 import Data.Data (Data, Typeable)
+import GHC.Exts (fromString)
 import GHC.Generics (Generic)
 import qualified Network.Wreq as Wreq
+import Paxos ( Acceptor(..), PaxosAction(Accept), Round(..) )
 import System.Console.CmdArgs (cmdArgs)
 import System.IO (withFile)
 import qualified Web.Scotty as Scotty
@@ -26,12 +30,52 @@ getClusterPorts path = do
   return $ map (\x -> read x :: Int) $ lines contents
 
 data PaxosResponse = PaxosResponse
-  {logIndex :: Int, yourRound :: Int, currentRound :: Int, currentAccepted :: Maybe Int}
+  {yourRound :: Int, currentRound :: Int, currentAccepted :: Maybe Int}
   deriving (Show, Generic, ToJSON, FromJSON)
+
+proposer :: MVar Round -> [Int] -> Int -> MVar Acceptor -> IO ()
+proposer round clusterPorts quorumSize paxos =
+  forever $ do
+    putStr "Input a value to propose: "
+    proposeValue <- readLn :: IO Int
+
+    p <- takeMVar paxos
+    r <- takeMVar round
+
+    case acceptedValue p of
+      Just accepted -> putStrLn $ "Consensus value is already chosen: " <> show accepted
+      Nothing -> do
+        result <- propose r proposeValue clusterPorts quorumSize
+        case result of
+          Nothing -> putStrLn "No result of proposal"
+          Just result -> case result of
+            Accept acceptedRound acceptedValue -> case acceptedValue of
+              Just value -> return ()
+              Nothing -> return ()
+            _ -> return ()
+
+    putMVar paxos p
+    putMVar round r
+
+propose :: Round -> Int -> [Int] -> Int -> IO (Maybe PaxosAction)
+propose round value clusterPorts quorumSize = do
+  let responses = map propose clusterPorts
+  return Nothing
+  where
+    propose :: Int -> IO (Maybe PaxosAction)
+    propose port = do
+      let opts = Wreq.defaults & Wreq.param "round" .~ [fromString . show $ round]
+      r <- Wreq.asJSON =<< Wreq.getWith opts ("localhost:" <> show port <> "/propose") :: IO (Wreq.Response PaxosResponse)
+      print $ r ^. Wreq.responseBody
+      print "BODY HERE"
+      return Nothing
 
 main :: IO ()
 main = do
   args <- cmdArgs args
+
+  let nodeId = port args
+  putStrLn $ "Current node id: " <> show nodeId
 
   clusterPorts <- getClusterPorts $ clusterPortsFilePath args
   putStrLn $ "Cluster ports: " <> show clusterPorts
@@ -39,23 +83,24 @@ main = do
   let quorumSize = ceiling (toRational (length clusterPorts) / 2)
   putStrLn $ "Quorum size: " <> show quorumSize
 
-  forkIO . forever $ do
-    putStr "Input a value to propose: "
-    proposeValue <- readLn :: IO Int
-    print proposeValue
+  paxos <- newMVar (Acceptor {highestPromise = Nothing, acceptedValue = Nothing})
+  currentRound <- newMVar (Round {roundNode = nodeId, roundNum = 0})
+
+  forkIO $ proposer currentRound clusterPorts quorumSize paxos
 
   Scotty.scotty (port args) $ do
     Scotty.get "/propose" $ do
-      logIndex <- Scotty.param "logIndex" :: Scotty.ActionM Int
       round <- Scotty.param "round" :: Scotty.ActionM Int
-      Scotty.json $ PaxosResponse {logIndex = logIndex, yourRound = round, currentRound = round, currentAccepted = Nothing}
+      liftIO $ do
+        p <- takeMVar paxos
+        print $ show p
+        putMVar paxos p
+      Scotty.json $ PaxosResponse {yourRound = round, currentRound = round, currentAccepted = Nothing}
 
     Scotty.get "/accept" $ do
-      logIndex <- Scotty.param "logIndex" :: Scotty.ActionM Int
       round <- Scotty.param "round" :: Scotty.ActionM Int
       value <- Scotty.param "value" :: Scotty.ActionM Int
-      Scotty.json $ PaxosResponse {logIndex = logIndex, yourRound = round, currentRound = round, currentAccepted = Just value}
+      Scotty.json $ PaxosResponse {yourRound = round, currentRound = round, currentAccepted = Just value}
 
     Scotty.get "/commit" $ do
-      logIndex <- Scotty.param "logIndex" :: Scotty.ActionM Int
       Scotty.text "Ok"
--- app/Paxos.hs
+++ app/Paxos.hs
@@ -4,7 +4,7 @@ module Paxos where
 
 import Utils (splitInts)
 
-data Round = Round {roundNode :: Int, roundNum :: Int}
+data Round = Round {roundNode :: Int, roundNum :: Int} deriving (Eq, Ord)
 
 parseRound :: String -> Round
 parseRound s = Round {roundNode = id, roundNum = num}
@@ -16,6 +16,35 @@ instance Show Round where
   show :: Round -> String
   show r = show (roundNode r) <> ";" <> show (roundNum r)
 
-data Acceptor = Acceptor {round :: (Int, Int), value :: Maybe Int}
+data PaxosAction = Accept Round (Maybe Int) | Promise Round (Maybe Int) | Commit
 
-data PaxosLog = PaxosLog {}
+data Acceptor = Acceptor {highestPromise :: Maybe Round, acceptedValue :: Maybe Int} deriving (Show)
+
+handlePrepare :: Acceptor -> Round -> (Acceptor, Maybe PaxosAction)
+handlePrepare state newPromise =
+  case highestPromise state of
+    Nothing -> (state', Just promise)
+    Just promised ->
+      if promised < newPromise
+        then (state', Just promise)
+        else (state, Nothing)
+  where
+    state' = Acceptor {highestPromise = Just newPromise, acceptedValue = acceptedValue state}
+    promise = Promise newPromise Nothing
+
+handleAccept :: Acceptor -> Round -> Int -> (Acceptor, Maybe PaxosAction)
+handleAccept state promise value =
+  case highestPromise state of
+    Nothing -> (state', Just accept)
+    Just promised -> case compare promised promise of
+      LT -> (state', Just accept)
+      EQ -> case acceptedValue state of
+        Nothing -> (state', Nothing)
+        Just accepted ->
+          if accepted == value
+            then (state, Nothing)
+            else (state', Just accept)
+      GT -> (state, Nothing)
+  where
+    state' = Acceptor {highestPromise = Just promise, acceptedValue = Just value}
+    accept = Accept promise (Just value)
